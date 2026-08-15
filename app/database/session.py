02b3da1bb3112cfc14e714d50e2d1aa691534687
João Vitor <joaovitorcorreia05032000@gmail.com>
2026-08-15 13:11:58 -0300

    alterações

--- app/database/session.py
+++ app/database/session.py
@@ -14,7 +14,7 @@ SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 # Create engine with pool pre-ping to handle dropped connections
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
